ef46776baf965844c1dc96408a9d59c3f7fbf132
DPACT <denisnusman@gmail.com>
2025-12-25 15:36:48 +0300

    Add CI tests and build/publish workflows

--- tests/test_user.py
+++ tests/test_user.py
@@ -30,24 +30,6 @@ def test_get_unexisted_user():
     assert response.status_code == 404
     assert isinstance(response.json(), dict)
 
-def test_create_user_with_valid_email():
-    '''Создание пользователя с уникальной почтой'''
-    new_user = {
-        'name': 'Anna Sokolova',
-        'email': 'a.sokolova@mail.com'
-    }
-    response = client.post("/api/v1/user", json=new_user)
-    assert response.status_code in (200, 201)
-    created = response.json()
-
-    # Обработка случая, когда сервер возвращает просто число (id)
-    if isinstance(created, int):
-        created = {'id': created}
-
-    assert 'id' in created
-    assert created.get('name') == new_user['name']
-    assert created.get('email') == new_user['email']
-
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
     duplicate = {
